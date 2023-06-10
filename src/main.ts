d41073418c98db0cc8f6680491c9e8a59301029c
mohamed magdy <mohamed.magdy0598@gmail.com>
2023-06-10 23:15:55 +0300

    refactor: :recycle: Lesson 2: Basic Types
    Basic types: string, boolean, number, any , arguments types, typescript detect implicitly return type and RegExp Type

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-let username = 'aSDDDDDDddddasdaslkdjaklsd';
-console.log(username);
+let myName: string = 'Mohamed Magdy'; // explicitly String Type Cannot be changed
+let myAge: number = 25; // explicitly number Type Cannot be changed
+let isSingle: boolean = 1 || true; // boolean type (we can't assign number for example)
+let isActive: boolean | number = 1 || true; // union Type (we can assign number)
 
-let a = 3;
-let b = '6';
-let c = 2;
+let mixed: any = 'das' || 20 || true || null || undefined; // any type is allowed Turn off TypeScript
 
-console.log();
+/* 
+function sum(a, b) {
+  // inferred to "any" type (implicitly has "any" type)
+  return a + b;
+}
+*/
+
+function sum(a: number, b: number) {
+  // explicitly has "number" type
+  return a + b;
+}
+
+let re: RegExp = /\w+/g; // Regular Expression Type
